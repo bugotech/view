6d132953be3741573faea2ee50dacb32c17db32e
Bruno Goncalves <bugotech@gmail.com>
2017-06-20 00:11:13 -0300

    configure

--- src/ViewServiceProvider.php
+++ src/ViewServiceProvider.php
@@ -1,6 +1,5 @@
 <?php namespace Bugotech\View;
 
-use Illuminate\Support\Arr;
 use Illuminate\View\FileViewFinder;
 
 class ViewServiceProvider extends \Illuminate\View\ViewServiceProvider
@@ -13,32 +12,10 @@ class ViewServiceProvider extends \Illuminate\View\ViewServiceProvider
     public function registerViewFinder()
     {
         $this->app->bind('view.finder', function ($app) {
-            $paths = $this->preparePaths();
+            $this->app->configure('view', __DIR__ . '/../config/view.php');
+            $paths = $app['config']['view.paths'];
 
             return new FileViewFinder($app['files'], $paths);
         });
-    }
-
-    /**
-     * Preparar pastas e retornar a lista de paths.
-     * @return array
-     */
-    protected function preparePaths()
-    {
-        $info = $this->app['files']->getRequire(__DIR__ . '/../config/view.php');
-
-        $paths = $this->app['config']['view.paths'];
-        if (is_null($paths)) {
-            $paths = Arr::get($info, 'paths', []);
-            $this->app['config']->set('view.paths', $paths);
-        }
-
-        $compiled = $this->app['config']['view.compiled'];
-        if (is_null($compiled)) {
-            $compiled = Arr::get($info, 'compiled', []);
-            $this->app['config']->set('view.compiled', $compiled);
-        }
-
-        return $paths;
     }
 }
